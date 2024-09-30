54d3fc3519c329e3f4ec62e814583b6e33069ba1
Malay Kumar <malaykumar2003@gmail.com>
2024-09-30 10:50:58 +0530

    Feat: Added Favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import "/styles/globals.css";
 
 export const metadata = {
   title: "ConsumeWise",
   description:
     "ConsumeWise is an AI-enabled smart label reader that helps consumers understand the health impact of packaged food products and nudges them to make better choices.",
+  icons: {
+    icon: "/assets/consumewise-favicon-color.png",
+  },
 };
 
 export default function RootLayout({ children }) {
